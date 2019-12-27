c4efd051e87d4eed44d1701f0597b98264076a5a
limengfan <limenfan@cmiot.chinamobile.com>
2019-12-27 17:35:43 +0800

    todo 创建接口界面

--- doc_module/js/create_doc_main.js
+++ doc_module/js/create_doc_main.js
@@ -2,21 +2,7 @@ const electron = require("electron");
 const resultObject = electron.remote.getGlobal('resultObject');
 const BrowserWindow = electron.remote.BrowserWindow;
 
-let interfaceIndex = 2;
-// document.getElementById("add_interface").onclick = function () {
-//     let interfaceList = document.getElementById("interfaceList");
-//     console.log(interfaceList);
-//     let li = document.createElement("li");
-//     let button = document.createElement("input");
-//     button.setAttribute("type", "button");
-//     button.setAttribute("id", "interface" + interfaceIndex)
-//     button.setAttribute("value", "接口" + interfaceIndex);
-//     li.appendChild(button)
-//     interfaceList.appendChild(li);
-//     li = null;
-//     button = null;
-//     interfaceIndex++;
-// }
+let interfaceCount = 1;
 
 document.getElementById("foreword").onclick = function () {
     let createDocWindow = new BrowserWindow({
@@ -40,33 +26,26 @@ document.getElementById("foreword").onclick = function () {
 }
 
 
-$(function () {
-    let $interfaceButton = $("button[id^='interface']");
-    $("#insert_interface").click(function () {
-        $("#interfaceList").append($("<li></li>").append($("<button></button>", {id: "interface" + interfaceIndex}).text("接口" + interfaceIndex)));
-        interfaceIndex++;
-        $interfaceButton = $("button[id^='interface']");
-        $interfaceButton.each(function (index, element) {
-            console.log($interfaceButton.length)
-            element.onclick = function () {
-                let interfaceWindow = new BrowserWindow({
-                    title: "fuck",
-                    width: 800,
-                    height: 600,
-                    webPreferences: {
-                        nodeIntegration: true
-                    }
-                });
-                interfaceWindow.loadFile("doc_module/interface.html").then(() =>
-                    interfaceWindow.on("close", function () {
-                        console.log(resultObject.foreword)
-                        interfaceWindow = null;
-                    }));
-                console.log(element);
-                console.log("haha");
-            }
-        });
-    })
+$("#insert_interface").click(function () {
+    $("#interfaceList").append($("<li></li>").append($("<button></button>", {id: "interface" + interfaceCount}).text("接口" + interfaceCount)));
+    let $interfaceButton = $("#interface" + interfaceCount);
+    let index = interfaceCount;
+    $interfaceButton.click(() => openCreateInterfacePage(index));
+    interfaceCount++;
+})
 
-
-});
+function openCreateInterfacePage(index) {
+    let interfaceWindow = new BrowserWindow({
+        title: "接口" + index,
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    interfaceWindow.loadFile("doc_module/interface.html").then(() =>
+        interfaceWindow.on("close", function () {
+            console.log(resultObject.foreword)
+            interfaceWindow = null;
+        }));
+}
